f50ff0a67c781e2d40c6232965d4be1fc25902f2
Shubhang <shubhang999454@gmail.com>
2024-10-16 09:46:00 +0530

    url changes

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -25,7 +25,7 @@ export const payment=async (req, res) => {
         },
       ],
       mode: 'payment',
-      success_url: 'http://localhost:5173/booking', // Adjust the success URL as per your app
+      success_url: 'https://airline-frontend-xi.vercel.app/booking', // Adjust the success URL as per your app
       cancel_url: 'http://localhost:5173/home', // Adjust the cancel URL as per your app
     });
    
